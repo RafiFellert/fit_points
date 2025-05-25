1c987aa9af537f480fca867981a93b6b5e59747c
Rafi Fellert <rafifellert@gmail.com>
2025-05-25 19:03:06 +0300

    Adding new file test/widget_test.dart

---
+++ test/widget_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gym_app_flutter/main.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    // Find the add icon button
+    final Finder addButton = find.byIcon(Icons.add);
+    expect(addButton, findsOneWidget);
+
+    // Tap the button and rebuild the widget
+    await tester.tap(addButton);
+    await tester.pump();
+
+    // Optionally verify updated text or state here
+  });
+}
